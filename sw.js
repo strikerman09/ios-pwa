8217782e4fc61cbab9cb1126eb61dbab08c39e31
strikerman09 <53001156+strikerman09@users.noreply.github.com>
2026-09-15 11:13:47 +0630

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,76 @@
+const CACHE_NAME = 'ios-pwa-dev-v1';
 
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys =>
+        Promise.all(
+          keys
+            .filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+        )
+      )
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  const request = event.request;
+
+  if (request.method !== 'GET') {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(request)
+      .then(cached => {
+
+        if (cached) {
+          return cached;
+        }
+
+        return fetch(request)
+          .then(response => {
+
+            if (
+              response &&
+              response.ok &&
+              new URL(request.url).origin === self.location.origin
+            ) {
+              const copy = response.clone();
+
+              caches.open(CACHE_NAME)
+                .then(cache => cache.put(request, copy))
+                .catch(() => {});
+            }
+
+            return response;
+          })
+          .catch(() => {
+
+            if (request.mode === 'navigate') {
+              return caches.match('./index.html');
+            }
+
+            return new Response('', {
+              status: 503,
+              statusText: 'Offline'
+            });
+          });
+      })
+  );
+});
